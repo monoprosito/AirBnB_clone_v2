27c6521cccc26a3ab13374234325e56e79ecce1f
jdrestre <jdrestre79@gmail.com>
2020-04-01 10:15:54 -0500

    Update Amenity: (models/amenity.py) with all class attributes requeriments of Task 10. DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    """This is the class for Amenity
+    """Represent an Amenity for a MySQL database.
+
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+
