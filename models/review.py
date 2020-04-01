78f0b37ef8fb408b42af4b71ec950553d0addc86
jdrestre <jdrestre79@gmail.com>
2020-04-01 08:57:35 -0500

    Update Review: (models/review.py) with all requeriments of Task 9. DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """This is the class for Review
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+
     Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+        place_id: (sqlalchemy String): The review's place id.
+        user_id: (sqlalchemy String): The review's place id.)
+        text: (sqlalchemy String): The review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
