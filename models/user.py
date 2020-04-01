93f7e8ed9e77ad05c60a9a3e888b287f38c28e74
jdrestre <jdrestre79@gmail.com>
2020-04-01 06:48:28 -0500

    Update User: (models/user.py) with requiriments class attribute in task 7

--- models/user.py
+++ models/user.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """This is the user class"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 
+class User(BaseModel, Base):
+    """ Represent a user for a MySQL database.
 
-class User(BaseModel):
-    """This is the class for user
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        __tablename__: represents the table name, users
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
