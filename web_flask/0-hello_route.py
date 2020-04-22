6de83ffafef965d324a34354b1a539280a8afe41
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-21 20:45:59 -0500

    Protects from double inclusion

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,13 @@
 """
 from flask import Flask
 
+if __name__ == '__main__':
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
 
-
-@app.route('/', strict_slashes=False)
-def index():
-    """Display 'Hello HBNB!'
-    """
-    return 'Hello HBNB!'
-
-
-app.run('0.0.0.0')
+    app.run('0.0.0.0')
