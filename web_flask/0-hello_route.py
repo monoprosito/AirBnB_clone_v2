f8f9319f15e3b64ab9c2478caae63f7f9f5ce2a7
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-04-21 19:52:19 -0500

    Starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Display 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+app.run('0.0.0.0')
