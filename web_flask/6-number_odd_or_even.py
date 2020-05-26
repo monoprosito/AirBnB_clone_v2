ec1c14ffd0ee9da8341ecd23eaee4afe6270ef44
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-26 19:15:30 +0000

    Fix another double inclusion protection

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,55 +4,16 @@
 from flask import Flask
 from flask import render_template
 
+app = Flask(__name__)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display a HTML page only if n is an integer
+    """
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def index():
-        """Display 'Hello HBNB!'
-        """
-        return 'Hello HBNB!'
-
-    @app.route('/hbnb', strict_slashes=False)
-    def hbnb():
-        """Display 'HBNB'
-        """
-        return 'HBNB'
-
-    @app.route('/c/<text>', strict_slashes=False)
-    def c(text):
-        """Display “C ” followed by the value of
-        the text variable (replace underscore _
-        symbols with a space)
-        """
-        return 'C ' + text.replace('_', ' ')
-
-    @app.route('/python/')
-    @app.route('/python/<text>', strict_slashes=False)
-    def python(text="is cool"):
-        """Display “Python ”, followed by the value of
-        the text variable (replace underscore _
-        symbols with a space )
-        """
-        return 'Python ' + text.replace('_', ' ')
-
-    @app.route('/number/<int:n>', strict_slashes=False)
-    def number(n):
-        """Display “n is a number” only if n is an integer
-        """
-        return str(n) + ' is a number'
-
-    @app.route('/number_template/<int:n>', strict_slashes=False)
-    def number_template(n):
-        """Display a HTML page only if n is an integer
-        """
-        return render_template('5-number.html', n=n)
-
-    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-    def number_odd_or_even(n):
-        """Display a HTML page only if n is an integer
-        """
-        parity = 'even' if n % 2 == 0 else 'odd'
-        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
-
     app.run('0.0.0.0')
