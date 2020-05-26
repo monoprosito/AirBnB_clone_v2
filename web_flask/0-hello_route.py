8cac2fedeb7d1e33afb2a7ec60b7623cdaffa51f
Santiago Arboleda Londoño <monoprosito@gmail.com>
2020-05-26 03:34:23 +0000

    Fix the scope of the double inclusion protection

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,16 @@
 """
 from flask import Flask
 
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Display 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def index():
-        """Display 'Hello HBNB!'
-        """
-        return 'Hello HBNB!'
-
     app.run('0.0.0.0')
